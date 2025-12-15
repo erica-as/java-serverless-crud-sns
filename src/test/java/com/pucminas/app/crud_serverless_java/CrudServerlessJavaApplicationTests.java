5279033227e1796d816513ca6b0ade72b5d92125
erica-as <erica.asnt@hotmail.com>
2025-12-14 21:08:53 -0300

    estrutura inicial

---
+++ src/test/java/com/pucminas/app/crud_serverless_java/CrudServerlessJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucminas.app.crud_serverless_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudServerlessJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
